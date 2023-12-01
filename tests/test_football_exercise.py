cab0534ac1187ef06789c9a941fe61606a57b708
carlosjuan995 <meiliang.zhao@maastrichtuniversity.nl>
2023-12-01 02:24:10 +0100

    asks the user for the names of two football teams, and the score of one team followed by the score of the other team. Your program should output how many points each team gets (3 for a win,1 for a draw, 0 if they lose).

--- tests/test_football_exercise.py
+++ tests/test_football_exercise.py
@@ -1,16 +1,26 @@
 import pytest
 import football_exercise
 
-# single test
-def test_calculate_points_tie():
-    assert football_exercise.calculate_points(2, 2) == (1, 1)
 
-# # This test uses a pytest parametrize decorator . Match the defined variables with the test input .
-# # The different cases are defined as a list of tuples .
 @pytest.mark.parametrize('score_t1, score_t2, points_t1, points_t2', [
     (0, 0, 1, 1),
     (1, 0, 3, 0),
     (0, 1, 0, 3)
 ])
-def test_calculate_points(score_t1, score_t2, points_t1, points_t2):
-    assert football_exercise.calculate_points(score_t1, score_t2) == (points_t1, points_t2)
+def test_calculate_points_oop(score_t1, score_t2, points_t1, points_t2):
+    fs = football_exercise.FootballScorer('t1', 't2', score_t1, score_t2)
+    fs.calculate_points()
+    assert fs.points_team_1 == points_t1
+    assert fs.points_team_2 == points_t2
+
+
+@pytest.mark.parametrize('score_t1, score_t2, points_t1, points_t2', [
+    (0, 0, 1, 1),
+    (5, 0, 3, 0),
+    (1, 2, 0, 3)
+])
+def test_calculate_points_oop1(score_t1, score_t2, points_t1, points_t2):
+    fs = football_exercise.FootballScorer('t1', 't2', score_t1, score_t2)
+    fs.calculate_points()
+    assert fs.points_team_1 == points_t1
+
